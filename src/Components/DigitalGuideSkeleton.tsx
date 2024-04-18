bd1422e836334c0276a40cadeb36b00ad7983743
mohamedsaeid1998 <105775732+mohamedsaeid1998@users.noreply.github.com>
2024-04-18 06:14:19 +0200

    Deploy

--- src/Components/DigitalGuideSkeleton.tsx
+++ src/Components/DigitalGuideSkeleton.tsx
@@ -3,12 +3,11 @@ interface IProps {
 
 }
 
-const DigitalGuideSkeleton = ({}: IProps) => {
+const DigitalGuideSkeleton = ({ }: IProps) => {
   return <>
     <div className="grid grid-cols-3 w-[70%] m-auto gap-10 justify-center ">
-    <div className="  w-[412px] h-[250px] rounded-md animate-pulse bg-gray-500 ">
-      dsaasdasdasdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-    </div>
+      {Array.from({ length: 6 }, (_, idx) => <div key={idx} className="  w-[412px] h-[250px] rounded-md animate-pulse bg-gray-500 ">
+      </div>)}
     </div>
   </>
 }
