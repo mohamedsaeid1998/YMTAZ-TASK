83ff5bcc891d9f15f50d4efd5c997e0b13179ed6
mohamedsaeid1998 <105775732+mohamedsaeid1998@users.noreply.github.com>
2024-04-18 07:57:35 +0200

    fix bugs

--- src/Components/CardDigitalGuide.tsx
+++ src/Components/CardDigitalGuide.tsx
@@ -10,9 +10,9 @@ const Card = ({lawyers_count ,title}: IDigitalGuideResponse) => {
 
   return <>
   
-    <div className="flex flex-col items-center justify-center bg-white px-20 pt-8 pb-10 rounded-md shadow-md ">
+    <div className="flex flex-col items-center justify-center bg-white px-20 pt-8 pb-10 rounded-md shadow-md card ">
 <div  className="bg-white rounded-full shadow-sm p-9 cycle ">
-<Pencil color="#DDB762" size={30} />
+<Pencil color="#DDB762" size={30}  />
 </div>
 <h3 className="text-subSecondaryColor font-semibold text-lg my-3">{title}</h3>
 <span className="text-[#6F6863]">  متوفر عدد  {lawyers_count}</span>
